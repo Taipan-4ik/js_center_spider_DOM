00901d9e74d87d60bc371318e4d87bc2e225ab36
Ivan Huzhva <iwan_guzhwa@gmail.com>
2025-03-19 15:48:55 +0100

    add task solution v2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,11 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', () => {
-  spiderOnCenter();
-});
-
 function spiderOnCenter() {
-  const wall = document.querySelector('.wall');
   const spider = document.querySelector('.spider');
 
-  spider.style.position = 'static';
-  wall.style.display = 'flex';
-  wall.style.justifyContent = 'center';
-  wall.style.alignItems = 'center';
+  spider.style.top = `50%`;
+  spider.style.left = `50%`;
+  spider.style.transform = `translate(-50%, -50%)`;
 }
 
 spiderOnCenter();
